d688aa059ef8abb04d60771bd3ebceef7a7aa99b
Milton Ossamu Tanizaka <milton.ossamu@gmail.com>
2024-09-14 19:39:09 -0300

    adding .env

--- database.py
+++ database.py
@@ -1,11 +1,15 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
+from dotenv import load_dotenv
 
 # Create a sqlite engine instance
-SQLALCHEMY_DATABASE_URL = "postgresql://usuarios_k6ts_user:PexK3uCvlxJibK4mM5t3hXmx5VSzYtib@dpg-criuk1rv2p9s738p091g-a.oregon-postgres.render.com/usuarios_k6ts"
-# SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root@localhost:3306/usuarios"
+load_dotenv()
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+DATABASE_URL = os.getenv('DATABASE_URL')
+LOCALHOST_URL = os.getenv('LOCALHOST_URL')
+
+engine = create_engine(DATABASE_URL)
 Base = declarative_base()
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
